db8b857e484668d02bf970f2e169b0c2ae90fca5
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-10-30 15:00:10 +0100

    use tick

--- src/test/scala/de/is24/aiwald/AIWaldSpec.scala
+++ src/test/scala/de/is24/aiwald/AIWaldSpec.scala
@@ -1,11 +1,9 @@
 package de.is24.aiwald
 
 import de.is24.aiwald.MapLoader.GameMap
-import org.newdawn.slick.GameContainer
-import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 
-class AIWaldSpec extends Specification with Mockito {
+class AIWaldSpec extends Specification {
   class TestAI extends AI {
 
     def nextMove(map: GameMap, playerLocation: PlayerLocation): Move = {
@@ -19,12 +17,11 @@ class AIWaldSpec extends Specification with Mockito {
   "Level 1" should {
     "be winnable by TestAI in 14 turns" in {
       val game = new Game(MapLoader.load("level_1"), new TestAI())
-      val gameContainer = smartMock[GameContainer]
       for (x ← 1 to 13) {
-        game.update(null, 10000)
+        game.tick()
         game.won must beFalse
       }
-      game.update(null, 10000) // this should pick up the coin
+      game.tick() // this should pick up the coin
       game.won must beTrue
       ok
     }
