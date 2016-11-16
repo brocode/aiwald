44b710625f91d2cc503a2d11a346c8a48c28f2ce
Patrick Haun <bomgar85@googlemail.com>
2016-11-16 09:37:09 +0100

    do nothing

--- src/main/scala/de/is24/aiwald/AI.scala
+++ src/main/scala/de/is24/aiwald/AI.scala
@@ -1,6 +1,7 @@
 package de.is24.aiwald
 
 import de.is24.aiwald.MapLoader.GameMap
+import scala.util.Random
 
 trait AI {
   def nextMove(map: GameMap, playerLocation: PlayerLocation): Move
@@ -19,9 +20,6 @@ trait AI {
 class MyAI extends AI {
 
   def nextMove(map: GameMap, playerLocation: PlayerLocation): Move = {
-    if (map(playerLocation.y)(playerLocation.x) == Tile.Coin)
-      Move.PICK_UP
-    else
-      Move.MOVE_FORWARD
+    Move.DO_NOTHING
   }
 }
