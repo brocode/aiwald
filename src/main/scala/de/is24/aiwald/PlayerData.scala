f5185add08acdcf378f82c3a45219be24e4f49f6
Patrick Haun <bomgar85@googlemail.com>
2016-12-13 16:04:50 +0100

    pimp AI API

--- src/main/scala/de/is24/aiwald/PlayerData.scala
+++ src/main/scala/de/is24/aiwald/PlayerData.scala
@@ -5,12 +5,8 @@ import Orientation._
 
 case class PlayerData(x: Int, y: Int, orientation: Orientation, hasSword: Boolean = false) {
   def moveForward(): PlayerData = {
-    orientation match {
-      case North ⇒ this.copy(y = y - 1)
-      case East  ⇒ this.copy(x = x + 1)
-      case South ⇒ this.copy(y = y + 1)
-      case West  ⇒ this.copy(x = x - 1)
-    }
+    val (x, y) = coordinatesInFrontOfPlayer
+    this.copy(x = x, y = y)
   }
 
   def isValidLocation(map: GameMap): Boolean = {
@@ -19,6 +15,18 @@ case class PlayerData(x: Int, y: Int, orientation: Orientation, hasSword: Boolea
   }
 
   def coordinatesInFrontOfPlayer: (Int, Int) = {
+    coordinatesInOrientation(orientation)
+  }
+
+  def coordinatesLeftFrontOfPlayer: (Int, Int) = {
+    coordinatesInOrientation(leftOrientation)
+  }
+
+  def coordinatesRightFrontOfPlayer: (Int, Int) = {
+    coordinatesInOrientation(rightOrientation)
+  }
+
+  private def coordinatesInOrientation(orientation: Orientation) = {
     orientation match {
       case Orientation.North ⇒ (x, y - 1)
       case Orientation.South ⇒ (x, y + 1)
@@ -27,21 +35,32 @@ case class PlayerData(x: Int, y: Int, orientation: Orientation, hasSword: Boolea
     }
   }
 
-  def rotateLeft(): PlayerData = orientation match {
-    case North ⇒ this.copy(orientation = West)
-    case East  ⇒ this.copy(orientation = North)
-    case South ⇒ this.copy(orientation = East)
-    case West  ⇒ this.copy(orientation = South)
+  private def leftOrientation: Orientation = {
+    orientation match {
+      case North ⇒ West
+      case East  ⇒ North
+      case South ⇒ East
+      case West  ⇒ South
+    }
   }
 
-  def rotateRight(): PlayerData = orientation match {
-    case North ⇒ this.copy(orientation = East)
-    case East  ⇒ this.copy(orientation = South)
-    case South ⇒ this.copy(orientation = West)
-    case West  ⇒ this.copy(orientation = North)
+  private def rightOrientation: Orientation = {
+    orientation match {
+      case North ⇒ East
+      case East  ⇒ South
+      case South ⇒ West
+      case West  ⇒ North
+    }
   }
+
+  def rotateLeft(): PlayerData =
+    this.copy(orientation = leftOrientation)
+
+  def rotateRight(): PlayerData =
+    this.copy(orientation = rightOrientation)
 }
 
 object PlayerData {
   val ValidLocationTiles = Seq(Tile.Grass, Tile.Coin, Tile.StartingArea, Tile.Sword)
+
 }
